e2d913f2a3ac35863e2beacc8d53080680191636
probison <parker.robison@hotmail.com>
2020-07-13 22:54:17 -0600

    added endpoints for single-search, search all, and delete functions

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,13 +8,95 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+// connect to SQLite database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+        return console.error(err.message);
+    }
+
+    console.log('Connected to the election database.');
+});
+
+// get all candidates
+// get () retrieves all the candidates from the candidates table.
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    // all () to retrieve all the rows.
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// Get single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates 
+                 WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+//delete a candidate
+//run method executes the SQL query but won't retrieve any result data. 
+// the ? is a prepared statement, or a placeholder that will be dynamically filled.
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        });
+    });
+});
+
+   
+
+// // Create a candidate 
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES (?,?,?,?)`;
+// //SQL params
+// const params = [1, 'Ronald', 'Firbank', 1];
+// // ES5 function not arrow, to use this
+// db.run(sql, params, function(err, result) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result, this.lastID);
+// });
+
 // Default response for any other request(Not Found) Catch all
 // This route should be last. It will override all others. 
 app.use((req, res) => {
     res.status(404).end();
 });
 
-// function that starts express.js server. 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// function that starts express.js server.
+// start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}); 
