ef1af04dbbb92b0a20c677f8a98b87fe201b22a0
probison <parker.robison@hotmail.com>
2020-07-15 00:38:58 -0600

    created api paths for party requests and to update party id

--- server.js
+++ server.js
@@ -106,6 +106,81 @@ app.post('/api/candidate', ({ body }, res) => {
     });
 });
 
+//update a candidate's party id
+app.put('/api/candidate/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'party_id');
+
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    
+    const sql = `UPDATE candidates SET party_id = ?
+                WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: req.body,
+            changes: this.changes
+        });
+    });
+});
+
+// to get all of the parties.
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// to get a party
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({ 
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// deletes a party
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = req.params.id;
+    db.run(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
+
+        res.json({ message: 'sucessfully deleted', changes: this.changes });
+    });
+});
+
 // Default response for any other request(Not Found) Catch all
 // This route should be last. It will override all others. 
 app.use((req, res) => {
